98e43a10bf52c069959d2654df11bfc8ebd03498
Dionne Rodriguez <DionneRodriguez.SE@gmail.com>
2019-07-28 18:41:46 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+switch (expression) {
+  case expression:
+
+    break;
+  default:
+    
+}// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://Rodriguez:xf3gpe6cl2zAcL50@cluster0-mijgb.mongodb.net/test?retryWrites=true&w=majority", // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'Savage_Authentication'
+};
